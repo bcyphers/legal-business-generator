5c77e368d87bde2eb14082e225f481d460ca87b5
Bennett Cyphers <bennett@eff.org>
2020-01-23 14:44:01 -0800

    initial commit

---
+++ generate.py
@@ -0,0 +1,13 @@
+# Totally Legal Business Generator v. 1.0
+
+strings = ['A company that uses <data type> about <demographic> to <purpose>',
+           'A company that sells <data type> about <demographic> to <entity>',
+           'A company that collects <data type> about <demographic> for <law enforcement agency>',
+           'A company that buys <data type> about <demographic> from <other business>']
+
+data_types = ['location data', 'credit data', 'employment history data', 'personality profiles', 're-identified health data', 'internet browsing history', 'dating app usage data', 'online purchase history', 'retail purchase history', 'driving behavior']
+demographics = ['pregnant women', 'single gay men', 'young democrats', 'church-going catholics', 'people with bad credit', 'people with mental health concerns', 'conspiracy theorists', 'everyone', 'the unemployed', 'people with pets', 'teenagers', 'people with gambling addictions', 'the homeless', 'single mothers', 'lonely old men']
+purposes = ['target ads', 'detect ToS violations', 'augment customer profiles', 'identify upsell targets', 'train predictive models', 'identify market inefficiencies', 'monitor competing businesses']
+entities = ['private investigators', 'reposession agencies', 'debt collectors', 'advertisers', 'political campaigns', 'local governments', 'hedge funds']
+other_businesses = ['grocery stores', 'car companies', 'gyms', 'failing startups', 'retailers', 'TV manufacturers', 'IoT companies', 'app developers', 'website publishers', 'electronic billboards']
+law_enforcement = ['the FBI', 'ICE', 'INTERPOL', 'state and local police']
