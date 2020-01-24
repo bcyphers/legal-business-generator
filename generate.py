fbf0470ccbb6f834c186c311efd75bfc1ecc4b9b
Bennett Cyphers <bennett@eff.org>
2020-01-23 17:50:50 -0800

    make it work

--- generate.py
+++ generate.py
@@ -1,13 +1,48 @@
 # Totally Legal Business Generator v. 1.0
+import random
+import re
 
-strings = ['A company that uses <data type> about <demographic> to <purpose>',
-           'A company that sells <data type> about <demographic> to <entity>',
-           'A company that collects <data type> about <demographic> for <law enforcement agency>',
-           'A company that buys <data type> about <demographic> from <other business>']
+strings = ['A company that uses %data_types data from %demographics to %purposes',
+           'A company that sells %data_types data about %demographics to %entities',
+           'A company that collects %data_types data about %demographics for %law_enforcement',
+           'A company that buys %data_types data about %demographics from %other_businesses']
 
-data_types = ['location data', 'credit data', 'employment history data', 'personality profiles', 're-identified health data', 'internet browsing history', 'dating app usage data', 'online purchase history', 'retail purchase history', 'driving behavior']
-demographics = ['pregnant women', 'single gay men', 'young democrats', 'church-going catholics', 'people with bad credit', 'people with mental health concerns', 'conspiracy theorists', 'everyone', 'the unemployed', 'people with pets', 'teenagers', 'people with gambling addictions', 'the homeless', 'single mothers', 'lonely old men']
-purposes = ['target ads', 'detect ToS violations', 'augment customer profiles', 'identify upsell targets', 'train predictive models', 'identify market inefficiencies', 'monitor competing businesses']
-entities = ['private investigators', 'reposession agencies', 'debt collectors', 'advertisers', 'political campaigns', 'local governments', 'hedge funds']
-other_businesses = ['grocery stores', 'car companies', 'gyms', 'failing startups', 'retailers', 'TV manufacturers', 'IoT companies', 'app developers', 'website publishers', 'electronic billboards']
-law_enforcement = ['the FBI', 'ICE', 'INTERPOL', 'state and local police']
+dic = {
+    'data_types': ['location', 'credit', 'employment history',
+                  'personality profile', 're-identified health',
+                  'internet browsing', 'dating app usage',
+                  'online purchase', 'retail purchase history',
+                  'driving behavior', 'pornography viewing history', 'DNA'],
+    'demographics': ['pregnant women', 'single gay men', 'young democrats',
+                    'church-going catholics', 'people with bad credit',
+                    'people with mental health issues', 'conspiracy theorists',
+                    'everyone', 'the unemployed', 'crazy cat people', 'teenagers',
+                    'people with gambling addictions', 'the homeless',
+                    'single mothers', 'lonely men', 'undocumented immigrants',
+                     'likely voters'],
+    'purposes': ['target ads', 'detect ToS violations', 'augment customer profiles',
+                'identify upsell targets', 'train predictive models',
+                'identify market inefficiencies', 'monitor competing businesses',
+                'perform A/B testing', 'make political predictions',
+                'forecast the price of oil'],
+    'entities': ['private investigators', 'reposession agencies', 'debt collectors',
+                'advertisers', 'political campaigns', 'local governments',
+                'hedge funds', 'subprime lenders', 'the Church of Scientology',
+                'foreign governments', 'drug companies', 'Big Tobacco',
+                'used car dealerships', 'landlords', 'the Republican Party'],
+    'other_businesses': ['grocery stores', 'car companies', 'gyms',
+                        'failing startups', 'retailers', 'TV manufacturers',
+                        'IoT companies', 'independent app developers',
+                        'mommy blogs', 'electronic billboard operators',
+                        'real estate agents', 'menstruation tracking apps',
+                        'weather apps'],
+    'law_enforcement': ['the FBI', 'ICE', 'INTERPOL', 'state and local police']
+}
+
+company = random.choice(strings)
+for l in dic:
+    company = re.sub('(?<=%)' + l, random.choice(dic[l]), company)
+
+company = ''.join(company.split('%'))
+
+print(company)
